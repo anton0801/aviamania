1cf92bfd0bf18f6aee3a8aac6fa7d41d9df20535
Anton <danilovanton724@gmail.com>
2024-03-25 17:05:16 +0100

    Initial Commit

---
+++ Avia Mania/Avia_ManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avia_ManiaApp.swift
+//  Avia Mania
+//
+//  Created by Anton on 25/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Avia_ManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
